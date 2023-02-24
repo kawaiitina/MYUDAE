40dd37cf2c5708008f78d597150bb83a8d8883b2
kawaiitina <39562943+kawaiitina@users.noreply.github.com>
2023-02-24 18:59:58 +0900

    레이아웃 수정

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -87,7 +87,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div ref="view" style="width: 1920px; height: 1080px">
+  <div ref="view" style="width: 1920px; height: 1080px; margin-bottom: 120px">
     <YouTube
       :src="score?.src || ''"
       width="1920"
